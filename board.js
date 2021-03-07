77a8ad61ffc68f3484264074c089aa0e61e36c3d
Hank Magan <bryant.magan@gmail.com>
2021-03-07 16:18:32 -0500

    finished game

--- board.js
+++ board.js
@@ -1,12 +1,34 @@
 class Board {
-    constuctor(){
-        this.board = new Array(20);
-        for(let i = 0; i < 20; i++){
-            let temp = new Array(20);
-            for(let j = 0; j < 20; j++){
-                temp[j] = 0;
+    constructor(){
+        this.food = {type: 1};
+        this.randomFood();
+    }
+
+    randomFood(body, length){
+        while(true){
+            this.food.x = Math.floor(Math.random() * 20) * 35;
+            this.food.y = Math.floor(Math.random() * 20) * 35;
+            let goodSpot = true;
+            for(let i = 0; i < length; i++){
+                if(body[i].x === this.food.x && body[i].y === this.food.y){
+                    goodSpot = false;
+                    break;
+                }
             }
-            this.board[i] = temp;
+            if(goodSpot){
+                break;
+            }
         }
+        document.getElementById("canvas").getContext("2d").fillStyle = "#FF0000";
+        document.getElementById("canvas").getContext("2d").fillRect(this.food.x, this.food.y, 30, 30);
+    }
+
+    gameOver(points){
+        let context = document.getElementById("canvas").getContext("2d");
+        context.fillStyle = "#000000";
+        context.font = "bold 50px Monospace";
+        context.fillText("Game over!", 220, 350);
+        context.font = "bold 30px Monospace";
+        context.fillText("Points: " + points, 280, 400);
     }
 }
--- snake.js
+++ snake.js
@@ -3,26 +3,73 @@ class Snake {
         this.x = 0;
         this.y = 0;
         this.length = 1;
+        this.body = [{x: 0, y: 0, type: 2}];
         this.context = document.getElementById("canvas").getContext("2d");
+        this.dirx = 1;
+        this.diry = 0;
+        this.old = {x: 0, y: 0, type: 0};
+        this.end = false;
+        this.ate = false;
+        this.moved = false;
+
+        this.context.fillStyle = "#FFFFFF";
+        this.context.fillRect(this.x, this.y, 30, 30);
     }
 
     move(x, y){
-        x *= 2;
-        y *= 2;
-        if(this.x + x <= 700 && this.x + x > 0){
-            this.x += x;
+        if(x != this.dirx * -1){
+            this.dirx = x;
+            this.moved = true;
         }
-        if(this.y + y <= 700 && this.y + y > 0){
-            this.y += y;
+        if(y != this.diry * -1){
+            this.diry = y;
+            this.moved = true;
         }
-        console.log("MOVING");
     }
 
-    update(){
+    isFinished(){
+        return this.end;
+    }
+
+    getAlreadyMoved(){
+        return this.moved;
+    }
+
+    update(board){
+        this.x += 35 * this.dirx;
+        this.y += 35 * this.diry;
+      
+        this.end = (this.x >= 700 || this.x < 0) || (this.y >= 700 || this.y < 0);
+        if(this.end){
+            return;
+        }
+        for(let i = 0; i < this.length; i++){
+            if(this.x === this.body[i].x && this.y === this.body[i].y){
+                this.end = true;
+                return;
+            }
+        }
+
+        this.body.push({x: this.x, y: this.y, type: 2});
+
+        this.ate = this.x === board.food.x && this.y === board.food.y;
+
+        if(!this.ate){
+            this.old = this.body.shift();
+            this.old.type = 0;
+        } else {
+            this.length++;
+            board.randomFood(this.body, this.length);
+        }
+
         this.context.fillStyle = "#FFFFFF";
         for(let i = 0; i < this.length; i++){
-            this.context.fillRect(this.x, this.y, 30, 30);
+            this.context.fillRect(this.body[i].x, this.body[i].y, 30, 30);
         }
-        console.log("UPDATING");
+
+        this.context.fillStyle = "#808080";
+        this.context.fillRect(this.old.x, this.old.y, 30, 30);
+
+        this.moved = false;
     }
 }
